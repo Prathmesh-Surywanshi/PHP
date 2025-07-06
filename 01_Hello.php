db0cb1ccc5dc828334ea578a0d524440f57d749c
Prathmesh-Surywanshi <prathameshsuryawanshipc@gmail.com>
2025-07-06 13:03:51 +0530

    initial commit 1st file added

---
+++ 01_Hello.php
@@ -0,0 +1,12 @@
+<?php
+ echo "Hello, World! 1 <br>";
+ echo 'Hello, World! 2 <br>';
+ echo (10+10)."\n";
+ echo (20+20),"<br><br>";
+ 
+ $Name = 'Prathmesh';
+ $num1=40; $num2=20;
+
+ echo "Name is $Name <br>";
+ echo (($num1 + $num2)."=Addition of $num1 and $num2")
+ ?>
