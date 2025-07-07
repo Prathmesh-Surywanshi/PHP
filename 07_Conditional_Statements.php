6ea05238d9163e32ed7d92a2886465bf267b2d85
Prathmesh-Surywanshi <prathameshsuryawanshipc@gmail.com>
2025-07-07 18:32:04 +0530

    Conditional Statements

---
+++ 07_Conditional_Statements.php
@@ -0,0 +1,25 @@
+<?php
+//1. If - else
+    $x = 12;
+    
+    if($x==0)
+    {
+        echo "The number is 0.";
+    }
+    else if($x>0)
+    {
+        echo "The number is positive.";
+    } else{
+        echo "The number is negative.";
+    }
+
+    //lesser number
+    $num1 = 20;
+    $num2 = 14;
+    if($num1<$num2){
+        echo"<br> Number 1 is lesser.";
+    } else {
+        echo"<br> Number 2 is lesser.";
+    }
+
+?>
