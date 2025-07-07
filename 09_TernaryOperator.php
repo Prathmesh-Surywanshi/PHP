6876647a827fa67f74c819ad5bd50ce4854f7fd8
Prathmesh-Surywanshi <prathameshsuryawanshipc@gmail.com>
2025-07-07 18:32:41 +0530

    Ternary Operator

---
+++ 09_TernaryOperator.php
@@ -0,0 +1,9 @@
+<?php
+    $x =-1;
+    echo ($x>0) ? "The number is positive." : "The number is negative.";
+
+    $num = 6;
+    $result = ($num%2)==0 ? "The number is even." : "The number is odd.";
+    // $result = ($num%2==0) ? "The number is even." : "The number is odd.";
+    echo "<br><br> $result";
+?>
