3d24eb359544e4fd011671165a31fb09b51ac0d4
Prathmesh-Surywanshi <prathameshsuryawanshipc@gmail.com>
2025-07-10 15:23:03 +0530

    Introspection Some Method Just List

---
+++ 30_Introspection.php
@@ -0,0 +1,11 @@
+<?php
+
+// examine an object characteristics it has methods like :
+    // 1.class_exists()
+    // 2.get_class_methods()
+    // 3.get_class_vars()
+    // 4.get_parent_class()
+    // 5.is_subclass_of()
+
+    // CODE REMAINING
+?>
