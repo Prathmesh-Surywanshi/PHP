26a6ab20537b0f30eee20e839740cc45d7880d52
Prathmesh-Surywanshi <prathameshsuryawanshipc@gmail.com>
2025-07-11 18:51:17 +0530

    Area Finding

---
+++ 36_Find-areas.php
@@ -0,0 +1,14 @@
+<!-- Q. Calculate area of Square, Circle and Rectangle. -->
+<?php
+
+    $length = 30;
+    $width = 8;
+    $side = 15;
+    $PI = 3.14;
+    $radius = 15;
+
+    echo "Area of Rectangle = ".($length*$width)."<br>";
+    echo "Area of Circle = ".($PI*$radius*$radius)."<br>";
+    echo "Area of Square = ",($side*$side);
+
+?>
